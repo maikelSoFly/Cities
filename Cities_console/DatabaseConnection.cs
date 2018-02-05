cd1770b83862a62f5a63e4fec4c867dae1eff299
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2018-02-05 13:16:10 +0100

    Initial commit

---
+++ Cities_console/DatabaseConnection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Cities_console
+{
+    public class DatabaseConnection
+    {
+        private String connectionString;
+        private String dbProvider;
+        private String dataSourcePath;
+        private String persistSecurityInfo;
+        private String password;
+        private OleDbConnection conn;
+
+
+        public DatabaseConnection(String sourcePath, String password, 
+                                  String provider="Microsoft.Jet.OLEDB.4.0;", 
+                                  String securityInfo="True;")
+        {
+            this.dataSourcePath = sourcePath;
+            this.password = password;
+            this.dbProvider = provider;
+            this.persistSecurityInfo = securityInfo;
+            this.conn = new OleDbConnection();
+            this.connectionString = @"Provider=" + this.dbProvider +
+                                    "Data Source=" + this.dataSourcePath +
+                                    "Persist Security Info=" + this.persistSecurityInfo +
+                                    "Jet OLEDB:Database Password=" + this.password;
+            conn.ConnectionString = this.connectionString;
+        }
+
+        //public static void Main() {}
+
+        public Boolean connect() {
+            try {
+                
+                conn.Open();
+
+            }
+            catch(Exception e) {
+                Console.WriteLine("OleDB connection FAILED.\n\t" + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Cities_console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Odbc;
+
+
+namespace Cities_console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            String path = "./db/Miasta.mdb";
+            DatabaseConnection dbConn = new DatabaseConnection(path, "");
+            if (dbConn.connect()) {
+                Console.WriteLine("DB connection established.");
+            }
+
+        }
+    }
+}
